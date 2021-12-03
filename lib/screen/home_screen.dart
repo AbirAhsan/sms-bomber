21552dd2de998c0d894cdcacc1ef084f3ecdbf83
Abir Ahsan <abirahsan122@gmail.com>
2021-12-03 22:36:16 +0600

    add tabbar screens

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,18 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:smsbomber/screen/tab_screen/for_multi_number.dart';
+import 'package:smsbomber/screen/tab_screen/for_single_number.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: const Text("SMS Bomber"),
-      ),
-      body: SizedBox(
-        child: Column(
-          children: [],
+    return DefaultTabController(
+      initialIndex: 0,
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          title: const Text("SMS Bomber"),
+          bottom: const TabBar(tabs: [
+            Tab(
+              child: Text("Single Number"),
+            ),
+            Tab(
+              child: Text("Multi Number"),
+            ),
+          ]),
+        ),
+        body: const SizedBox(
+          child: TabBarView(children: [
+            ForSingleNumber(),
+            ForMultiNumber(),
+          ]),
         ),
       ),
     );
