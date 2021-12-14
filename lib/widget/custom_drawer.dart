37b24bfd80bb68b20a2f29a768c44444adb943d8
Abir Ahsan <abirahsan122@gmail.com>
2021-12-14 13:02:55 +0600

    redesign drawer screen

--- lib/widget/custom_drawer.dart
+++ lib/widget/custom_drawer.dart
@@ -14,44 +14,56 @@ class CustomDrawer extends StatelessWidget {
       child: Container(
         height: _height,
         width: _width / 4,
-        child: Column(
-          children: [
-            const Text(
-              "This Project is under development",
-              style: TextStyle(
-                  fontSize: 32, color: Colors.red, fontWeight: FontWeight.bold),
-            ),
-            ListTile(
-              title: const Text(
-                "Contact Us",
-                style: TextStyle(fontSize: 20),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 30,
               ),
-              onTap: () async {
-                String fbProtocolUrl;
-                if (Platform.isIOS) {
-                  fbProtocolUrl = 'fb://profile/716134558805812';
-                } else {
-                  fbProtocolUrl = 'fb://page/716134558805812';
-                }
-
-                String fallbackUrl =
-                    'https://www.facebook.com/learnwithabirahsan';
-
-                try {
-                  bool launched =
-                      await launch(fbProtocolUrl, forceSafariVC: false);
-
-                  if (!launched) {
-                    await launch(fallbackUrl, forceSafariVC: false);
-                  }
-                } catch (e) {
-                  await launch(fallbackUrl, forceSafariVC: false);
-                }
-              },
-            )
-          ],
+              const Text(
+                "This Project is under development",
+                style: TextStyle(
+                    fontSize: 32,
+                    color: Colors.red,
+                    fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ListTile(
+                leading: const Icon(Icons.contact_page),
+                title: const Text(
+                  "Contact Us",
+                  style: TextStyle(fontSize: 20),
+                ),
+                onTap: () => contactUsService(),
+              )
+            ],
+          ),
         ),
       ),
     );
+  }
+}
+
+contactUsService() async {
+  String fbProtocolUrl;
+  if (Platform.isIOS) {
+    fbProtocolUrl = 'fb://profile/716134558805812';
+  } else {
+    fbProtocolUrl = 'fb://page/716134558805812';
+  }
+
+  String fallbackUrl = 'https://www.facebook.com/learnwithabirahsan';
+
+  try {
+    bool launched = await launch(fbProtocolUrl, forceSafariVC: false);
+
+    if (!launched) {
+      await launch(fallbackUrl, forceSafariVC: false);
+    }
+  } catch (e) {
+    await launch(fallbackUrl, forceSafariVC: false);
   }
 }
